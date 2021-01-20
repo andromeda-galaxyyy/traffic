9ce9a056e90c2d05da9ea8b02b972eed3638cb7f
吴环宇 <whyaiwxf@gmail.com>
2021-01-20 14:49:23 +0800

    add:telemetry api

--- api/telemetry.go
+++ api/telemetry.go
@@ -3,11 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
-	"net/http"
-	"strconv"
-
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v8"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
 )
 
 var (
@@ -70,6 +71,86 @@ func TelemetryFunc(isDelay bool) func(c *gin.Context)  {
 		return
 	}
 }
+
+// get stats of all link
+func TelemetryFunc2(isDelay bool) func(c *gin.Context) {
+	var keyTemplate string
+	if isDelay{
+		keyTemplate="*.delay"
+	}else{
+		keyTemplate="*.loss"
+	}
+	return func(c *gin.Context) {
+		res:=make(map[string]float64)
+		for i:=0;i<100;i++{
+			for j:=0;j<100;j++{
+				if i>=j{
+					continue
+				}
+				if !topo[i][j]{
+					continue
+				}
+				k:=fmt.Sprintf("%d-%d",i,j)
+				res[k]=0
+			}
+		}
+		ctx:=context.Background()
+		keys,err:=telemetryHandle.Keys(ctx,keyTemplate).Result()
+		if err!=nil{
+			c.JSON(http.StatusInternalServerError,internalErrorJSON)
+			return
+		}
+		for _,key:=range keys{
+			// 1-2.delay
+			//1-2.loss
+			//1-2
+			kk:=strings.Split(key,".")[0]
+			//fix if we meet 2-1
+			nodesStr:=strings.Split(kk,"-")
+			if len(nodesStr)!=2{
+				c.JSON(http.StatusInternalServerError,internalErrorJSON)
+				return
+			}
+			aa,bb:=nodesStr[0],nodesStr[1]
+			a,err:=strconv.Atoi(aa)
+			if err!=nil{
+				c.JSON(http.StatusInternalServerError,internalErrorJSON)
+				return
+			}
+			b,err:=strconv.Atoi(bb)
+			if err!=nil{
+				c.JSON(http.StatusInternalServerError,internalErrorJSON)
+				return
+			}
+			if a>b{
+				kk=fmt.Sprintf("%d-%d",b,a)
+			}
+			vals,err:=linkRateRedisHandle.ZRevRangeByScore(ctx,key,&redis.ZRangeBy{
+				Min: "-inf",
+				Max:"+inf",
+				Count: 1,
+			}).Result()
+			if err!=nil{
+				c.JSON(http.StatusInternalServerError,internalErrorJSON)
+				return
+			}
+			if len(vals)!=1{
+				log.Printf("warn: cannot find value of key:%s\n",key)
+			}
+			rate,err:=strconv.ParseFloat(vals[0],64)
+			if err!=nil{
+				c.JSON(http.StatusInternalServerError,internalErrorJSON)
+				return
+			}
+			res[kk]=rate
+		}
+		c.JSON(http.StatusOK,res)
+		return
+	}
+}
+
+
+
 //
 //func GetLinkDelay(c *gin.Context){
 //	u,ok:=c.GetQuery("u")
